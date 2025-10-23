fbde953ef7896aeed1e359c29cac1c412589b9b9
RuneRogue <priyankmakwana305@gmail.com>
2025-10-24 00:40:25 +0530

    updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import { APP_DESCRIPTION, APP_NAME, APP_URL, BRAND_COLOR, MAIN_FONT } from '../consts/app';
+import { APP_DESCRIPTION, APP_NAME, APP_URL, MAIN_FONT } from '../consts/app';
 
 export default function Document() {
   return (
@@ -7,12 +7,15 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        {/* Only load essential favicon synchronously */}
+        <link rel="icon" href="/favicon.ico" />
+        
+        {/* Load other icons asynchronously to not block rendering */}
+        <link rel="preload" as="image" href="/apple-touch-icon.png" />
+        <link rel="preload" as="image" href="/favicon-32x32.png" />
+        <link rel="preload" as="image" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color={BRAND_COLOR} />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
 
